bb8253c07c90539d84e4d7d2b7a9250fd7528067
Prakash Tiwari <prakashtiwari.net@gmail.com>
2021-03-27 15:38:06 +0530

    docker file added

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,51 @@
+﻿using Shopping.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+        new Product()
+        {
+        Name="IPhone X",
+        Description="This product is the best",
+        Price=100.20M,
+        Category="Smart Phone"
+        },
+        new Product()
+        {
+        Name="Samsung 10",
+        Description="This product is the best",
+        Price=100.20M,
+        Category="Smart Phone"
+        },
+        new Product()
+        {
+        Name="IPhone X",
+        Description="This product is the best",
+        Price=100.20M,
+        Category="Smart Phone"
+        },
+        new Product()
+        {
+        Name="LG Nexus",
+        Description="This product is the best",
+        Price=100.20M,
+        Category="Smart Phone"
+        },
+         new Product()
+        {
+        Name="HTC",
+        Description="This product is the best",
+        Price=1100.20M,
+        Category="Smart Phone"
+        }
+        };
+
+    }
+}
